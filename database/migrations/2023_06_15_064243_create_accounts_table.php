f1a8301f4dd21021ae5356df6054e6107470a7dd
chitosanny <zhidouz46@gmail.com>
2023-06-15 15:50:17 +0900

    migration

---
+++ database/migrations/2023_06_15_064243_create_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('password');
+            $table->string('mail');
+            $table->string('password_confirmation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
