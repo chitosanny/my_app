00967beea1984f1cad9e642e9f3ca179dc451853
chitosanny <zhidouz46@gmail.com>
2023-06-20 12:41:17 +0900

    blog投稿編集

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrap();  
         \URL::forceScheme('https'); //追加
         $this->app['request']->server->set('HTTPS','on');
     }
